d7b2d6a1d29313ef17fe06d302e3566ce8a9a5c9
omerc <omerc@DESKTOP-P7FDVCL>
2023-07-25 21:48:56 +0300

    4. commit

---
+++ app/src/main/java/com/example/challenge/Question4/Example.kt
@@ -0,0 +1,27 @@
+package com.example.challenge.Question4
+
+import java.util.Scanner
+
+fun checkVowel(input : String) : Boolean? {
+    val vowel = arrayListOf<String>("A","a","E","e","I","ı","İ","i","O","o","Ö","ö","U","u","Ü","ü")
+    var success:Boolean?=null
+    vowel.map{
+        if (it == input) success = true
+    }
+    return success
+}
+
+fun main(){
+
+
+    val  scanner = Scanner(System.`in`)
+
+    println("Harf giriniz.")
+    val input: String =scanner.next()
+
+    if (checkVowel(input) == true){
+        println("Sesli harftir.")
+    }
+    else println("Sessiz harftir.")
+
+}
