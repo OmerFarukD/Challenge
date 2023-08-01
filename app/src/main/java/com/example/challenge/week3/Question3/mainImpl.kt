c9e33022d9bafc0cbe024ab0a06e231e224f2263
omerc <omerc@DESKTOP-P7FDVCL>
2023-08-01 20:48:08 +0300

    week3 Commit

--- app/src/main/java/com/example/challenge/week3/Question3/mainImpl.kt
+++ app/src/main/java/com/example/challenge/week3/Question3/mainImpl.kt
@@ -1,10 +1,9 @@
 package com.example.challenge.week3.Question3
 
 fun birlestir(list1:ArrayList<Int>,list2:ArrayList<Int>): List<Int>{
-    var result = ArrayList<Int>()
      return (list1 + list2).sorted()
 }
 
 fun main() {
-
+print(birlestir(arrayListOf(1,2,3), arrayListOf(1,2,3)))
 }
