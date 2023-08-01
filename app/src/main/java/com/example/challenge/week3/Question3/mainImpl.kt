679408ca7e8196a831a69a0ce4ff9f142852c3d6
omerc <omerc@DESKTOP-P7FDVCL>
2023-08-01 20:46:39 +0300

    week3 Commit

---
+++ app/src/main/java/com/example/challenge/week3/Question3/mainImpl.kt
@@ -0,0 +1,10 @@
+package com.example.challenge.week3.Question3
+
+fun birlestir(list1:ArrayList<Int>,list2:ArrayList<Int>): List<Int>{
+    var result = ArrayList<Int>()
+     return (list1 + list2).sorted()
+}
+
+fun main() {
+
+}
